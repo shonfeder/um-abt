64292f2dc00929d46f046f40f47767a9b76ee04c
Shon Feder <shon.feder@gmail.com>
2021-08-06 22:09:03 -0400

    Improve property based tests

--- test/test.ml
+++ test/test.ml
@@ -7,17 +7,26 @@ let property name = Test.make ~count ~name
 let utlc_tests =
   let open Example.Untyped_lambda_calculus.Syntax in
   let ( = ) = equal in
+  let x, y, z = (v "x", v "y", v "z") in
+  let s = oneofl [ lam "x" (lam "y" (lam "z" (app (app x y) (app y z)))) ] in
+  let k = oneofl [ lam "x" (lam "y" x); lam "y" (lam "x" y) ] in
+  let i = oneofl [ lam "x" x; lam "y" y; lam "z" z ] in
+  let ski = oneof [ s; k; i ] in
+  let three x = triple x x x in
+  let two x = pair x x in
   let term = Abt_gen.Utlc.arbitrary in
   [ property "alpha equivalence -- reflexivity" term (fun t -> t = t)
   ; property
       "alpha equivalance -- compatibility"
-      (triple term term term)
+      (oneof [ three term; three ski ])
       (fun (l, m, n) -> m = n ==> (app m l = app n l && app l m = app l n))
-  ; property "alpha equivalence -- symmetry" (pair term term) (fun (m, n) ->
-        m = n ==> (n = m))
+  ; property
+      "alpha equivalence -- symmetry"
+      (oneof [ two term; two ski ])
+      (fun (m, n) -> m = n ==> (n = m))
   ; property
       "alpha equivalence -- transitivity"
-      (triple term term term)
+      (oneof [ three term; three ski ])
       (fun (l, m, n) -> (l = m && m = n) ==> (l = n))
   ]
 
