7f94cedf4241f43b8b6b40d4ee3aad8fdedeb3a8
Shon Feder <shon.feder@gmail.com>
2021-08-08 08:05:57 -0400

    Clean up Ultc example

--- test/example/example.ml
+++ test/example/example.ml
@@ -26,31 +26,29 @@ module Untyped_lambda_calculus = struct
     let show s = to_string s |> print_endline
   end
 
-  module Semantics = struct
-    (** Semantics for the simply typed lambda calculus *)
+  (* Evaluation for the simply typed lambda calculus *)
 
-    open Syntax
+  open Syntax
 
-    let rec eval : t -> t =
-     fun t ->
-      let var = Fun.const t in
-      let bnd = Fun.const t in
-      let opr = function
-        | Op.App (m, n) -> apply (eval m) (eval n)
-        | Op.Lam abs    -> op (Op.Lam abs)
-      in
-      case ~var ~bnd ~opr t
+  let rec eval : t -> t =
+    fun t ->
+    let var = Fun.const t in
+    let bnd = Fun.const t in
+    let opr = function
+      | Op.App (m, n) -> apply (eval m) (eval n)
+      | Op.Lam abs    -> op (Op.Lam abs)
+    in
+    case ~var ~bnd ~opr t
 
-    and apply : t -> t -> t =
-     fun m n ->
-      let var _ = app m n in
-      let bnd (b, t) = subst b ~value:n t in
-      let opr = function
-        | Op.App (_, _) -> app m n
-        | Op.Lam bnd    -> eval (apply bnd n)
-      in
-      case ~var ~opr ~bnd m
-  end
+  and apply : t -> t -> t =
+    fun m n ->
+    let var _ = app m n in
+    let bnd (b, t) = subst b ~value:n t in
+    let opr = function
+      | Op.App (_, _) -> app m n (* Can't reduce any further *)
+      | Op.Lam bnd    -> eval (apply bnd n)
+    in
+    case ~var ~opr ~bnd m
 
   module Examples = struct
     open Syntax
@@ -65,14 +63,20 @@ module Untyped_lambda_calculus = struct
       [%expect {| (m n) |}]
 
     let%expect_test "Utlc evaluation" =
-      show @@ Semantics.eval (app (lam "x" x) m);
+      show @@ eval (app (lam "x" x) m);
       [%expect {| m |}];
 
-      show @@ Semantics.eval (app (lam "z" (app (lam "x" x) z)) m);
+      show @@ eval (app (lam "z" (app (lam "x" x) z)) m);
+      [%expect {| m |}];
+
+      let a = app (app (lam "z" z) (lam "z" z)) m in
+      show a;
+      [%expect {| (((λz.z) (λz.z)) m) |}];
+      show @@ eval a;
       [%expect {| m |}];
 
       show
-      @@ Semantics.eval
+      @@ eval
            (app (lam "z" (app (lam "x" x) z)) (app (lam "y" y) (lam "z" n)));
       [%expect {| (λz.n) |}]
 
